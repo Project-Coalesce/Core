c17fc2443bde9e16eef2f99fdccef2e872b0f642
Sxtanna <ranald.taylor@gmail.com>
2017-04-08 01:20:55 -0400

    Initial Commit

---
+++ src/main/java/com/coalesce/Core.java
@@ -0,0 +1,12 @@
+package com.coalesce;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Core extends JavaPlugin {
+
+
+	@Override
+	public void onEnable() {
+		// TODO: 4/8/2017 Enabled >:)
+	}
+}
