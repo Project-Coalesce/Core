c7763ce720557f80a31db2ed126d18f5477bc6a6
Sxtanna <ranald.taylor@gmail.com>
2017-04-10 20:36:29 -0400

    Updated core to contain commandModule

--- src/main/java/com/coalesce/Core.java
+++ src/main/java/com/coalesce/Core.java
@@ -1,18 +1,20 @@
 package com.coalesce;
 
-import com.coalesce.user.UserModule;
+import com.coalesce.command.CommandModule;
 import com.coalesce.plugin.CoPlugin;
+import com.coalesce.user.UserModule;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import static org.bukkit.plugin.ServicePriority.Normal;
 
 public final class Core extends CoPlugin {
     
-    //The core instance.
 	private static Core instance;
 
 
 	private UserModule userModule;
+	private CommandModule commandModule;
 
 
 	@Override
@@ -25,28 +27,37 @@ public final class Core extends CoPlugin {
 	public void onPluginEnable() {
 		getServer().getServicesManager().register(Core.class, this, this, Normal);
 
-		addModules(userModule = new UserModule(this));
+		addModules(userModule = new UserModule(this), commandModule = new CommandModule(this));
 	}
 
 	@Override
 	public void onPluginDisable() {
 		instance = null;
 		userModule = null;
+		commandModule = null;
 	}
 
 
-	public @Nullable UserModule getUserModule() {
-		return userModule;
+	public @NotNull UserModule getUserModule() {
+		return checkCoreEnabled(userModule);
 	}
-    
-    
-    /**
+
+	public @NotNull CommandModule getCommandModule() {
+		return checkCoreEnabled(commandModule);
+	}
+
+	/**
      * Grabs the instance of the core.
      * Make sure you don't call this before {@link #onPreEnable()} nor after {@link #onPluginDisable()}.
      *
      * @return The core instance.
      */
     public static @Nullable Core getInstance() {
+		return instance;
+	}
+
+	private <M> M checkCoreEnabled(M instance) {
+		if (!isEnabled()) throw new IllegalStateException("Core plugin is not enabled");
 		return instance;
 	}
 
