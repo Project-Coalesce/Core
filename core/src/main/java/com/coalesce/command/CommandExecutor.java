e2996c767ac216b701b975560f97e42a6e094339
NJDaeger <noahdaeger@hotmail.com>
2017-04-27 16:09:11 -0400

    Added lots o documentation. Gonna start working on more examples.

--- core/src/main/java/com/coalesce/command/CommandExecutor.java
+++ core/src/main/java/com/coalesce/command/CommandExecutor.java
@@ -1,7 +1,11 @@
 package com.coalesce.command;
 
 public interface CommandExecutor {
-
+	
+	/**
+	 * The command execution method.
+	 * @param commandMethod The method reference to a command.
+	 */
 	void execute(CommandContext commandMethod);
 
 }
