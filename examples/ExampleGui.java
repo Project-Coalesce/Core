e2007dac9c93fdc8795c5b05ef6aaa2892dd539f
Allen Lantz <allen.m.lantz@gmail.com>
2017-04-29 20:55:15 -0500

    Example Update

--- examples/ExampleGui.java
+++ examples/ExampleGui.java
@@ -1,8 +1,7 @@
-package example;
+package com.coalesce.gui;
 
+import com.coalesce.plugin.CoPlugin;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
-import com.coalesce.gui.*;
 
 /**
  * A contrived example to display the usage of {@link PlayerGui}.
@@ -10,21 +9,21 @@ import com.coalesce.gui.*;
 public class ExampleGui extends PlayerGui {
 
     private static final int SIZE = 9;
-    
-    public ExampleGui(JavaPlugin plugin) {
-        super(plugin, SIZE, "Shop"); 
-        
-        ItemStack[] items = new ItemStack[0]; // some series of item stacks to display 
+
+    public ExampleGui(CoPlugin plugin) {
+        super(plugin, SIZE, "Shop");
+
+        ItemStack[] items = new ItemStack[0]; // some series of item stacks to display
         for (ItemStack stack : items) {
-            
+
             setItem(0, player -> {
                 // can do per-player things here, like displaying their currency or some such
                 int money = 42; // someCurrencyApi.getBalance(someCurrency, player);
-                return new IconBuilder(stack)
-                        .name("Balance: " + money)
+                return new ItemBuilder(stack.getType())
+                        .displayName("Balance: " + money)
                         .lore(" ", "Click to buy", " ", "Costs x", " ", "You have $" + money)
-                        .build().getItemStack();
-            }, p -> {
+                        .build();
+            }, (event) -> {
                 // on click
                 // update user's balance, add item to inventory, etc.
             });
